a9d8b8f8d5a2ac840930f2f0497de617dbd27d9b
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2020-04-13 21:43:09 +0700

    Change fake checkbox buttons to Chakra ControlBox

--- src/features/life/menu/RuleCheckboxRow.js
+++ src/features/life/menu/RuleCheckboxRow.js
@@ -1,44 +1,75 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { Button, Flex, Text, Tooltip } from '@chakra-ui/core';
+import {
+  ControlBox,
+  VisuallyHidden,
+  Flex,
+  Text,
+  useColorMode,
+} from '@chakra-ui/core';
 
 export const RuleCheckboxRow = React.memo(
   ({ ruleArray, ruleType, onChange, ...rest }) => {
-    const numberString = ruleArray
-      .reduce((acc, val, idx) => (val ? acc.concat(idx) : acc), [])
-      .join(`|`);
+    const { colorMode } = useColorMode();
 
-    const tooltipLabel =
-      ruleType === 'born'
-        ? `neighbors == [${numberString || 'null'}] ~> born`
-        : `neighbors == [${numberString || 'null'}] ~> survive`;
+    // const numberString = ruleArray
+    //   .reduce((acc, val, idx) => (val ? acc.concat(idx) : acc), [])
+    //   .join(`|`);
+
+    // const tooltipLabel =
+    //   ruleType === 'born'
+    //     ? `neighbors == [${numberString || 'null'}] ~> born`
+    //     : `neighbors == [${numberString || 'null'}] ~> survive`;
 
     return (
-      <Tooltip label={tooltipLabel} {...rest} placement="top" zIndex={2}>
-        <Flex align="center" justify="space-between">
-          <Text fontWeight="300" fontSize="sm">
-            {ruleType === 'born' ? 'b ~>' : 's ~>'}
-          </Text>
+      <Flex align="center" justify="space-between">
+        <Text fontSize="sm">{ruleType === 'born' ? 'b ~>' : 's ~>'}</Text>
 
-          {ruleArray.map((val, index) => {
-            const key = `${ruleType}${index}`;
-
-            return (
-              <Button
-                key={key}
-                size="xs"
-                variant={ruleArray[index] ? 'solid' : 'ghost'}
-                onClick={() => {
+        {ruleArray.map((val, index) => {
+          const key = `${ruleType}${index}`;
+          const isChecked = ruleArray[index];
+          return (
+            <label key={key}>
+              <VisuallyHidden
+                as="input"
+                type="checkbox"
+                checked={isChecked}
+                onChange={() => {
                   onChange(ruleType, index);
                 }}
+              />
+
+              <ControlBox
+                borderWidth="1px"
+                size="1.5rem"
+                rounded="sm"
+                color={
+                  colorMode === 'light' ? 'blackAlpha.600' : 'whiteAlpha.600'
+                }
+                borderColor="transparent"
+                _child={{ opacity: 100 }}
+                _checked={
+                  colorMode === 'light'
+                    ? {
+                        bg: 'blue.500',
+                        color: 'gray.50',
+                        borderColor: 'blue.500',
+                      }
+                    : {
+                        bg: 'blue.200',
+                        color: 'gray.800',
+                        borderColor: 'blue.200',
+                      }
+                }
+                _focus={{ boxShadow: 'outline' }}
               >
-                {index}
-              </Button>
-            );
-          })}
-        </Flex>
-      </Tooltip>
+                <Text fontSize="sm">{index}</Text>
+              </ControlBox>
+            </label>
+          );
+        })}
+      </Flex>
     );
   }
 );
