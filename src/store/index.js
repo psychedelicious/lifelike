7e9e7f00a065dfa1b8f1b3b764a2b02149aacc6f
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2020-05-02 18:21:33 +0700

    Enable Redux devtools for staging branch

--- src/store/index.js
+++ src/store/index.js
@@ -5,18 +5,18 @@ import { persistStore } from 'redux-persist';
 // Due to the size of the cell state, the app really bogs down Redux devtools,
 // even for other apps that are using Redux. commenting it out for production build.
 
-// import { composeWithDevTools } from 'redux-devtools-extension';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
-// const composeEnhancers = composeWithDevTools({ trace: true, traceLimit: 25 });
-
-// export default () => {
-//   let store = createStore(rootReducer, composeEnhancers());
-//   let persistor = persistStore(store);
-//   return { store, persistor };
-// };
+const composeEnhancers = composeWithDevTools({ trace: true, traceLimit: 25 });
 
 export default () => {
-  let store = createStore(rootReducer);
+  let store = createStore(rootReducer, composeEnhancers());
   let persistor = persistStore(store);
   return { store, persistor };
 };
+
+// export default () => {
+//   let store = createStore(rootReducer);
+//   let persistor = persistStore(store);
+//   return { store, persistor };
+// };
