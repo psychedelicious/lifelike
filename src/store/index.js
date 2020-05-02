ff9fb1fe1b42f37b0f343d6d3735285a3ca84533
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2020-05-02 17:50:14 +0700

    Disable Redux Devtools for production build

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,22 @@
 import { createStore } from 'redux';
 import rootReducer from './reducers';
 import { persistStore } from 'redux-persist';
-import { composeWithDevTools } from 'redux-devtools-extension';
 
-const composeEnhancers = composeWithDevTools({ trace: true, traceLimit: 25 });
+// Due to the size of the cell state, the app really bogs down Redux devtools,
+// even for other apps that are using Redux. commenting it out for production build.
+
+// import { composeWithDevTools } from 'redux-devtools-extension';
+
+// const composeEnhancers = composeWithDevTools({ trace: true, traceLimit: 25 });
+
+// export default () => {
+//   let store = createStore(rootReducer, composeEnhancers());
+//   let persistor = persistStore(store);
+//   return { store, persistor };
+// };
 
 export default () => {
-  let store = createStore(rootReducer, composeEnhancers());
+  let store = createStore(rootReducer);
   let persistor = persistStore(store);
   return { store, persistor };
 };
