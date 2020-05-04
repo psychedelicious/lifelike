19d0eb95e6bb0a7f10a6ea68a4bc9166bf51b832
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2020-05-04 20:44:24 +0700

    Added stateSanitizer to redux devtools integration.
    Devtools now doesn't slow down terribly due to huge cells array.

--- src/store/index.js
+++ src/store/index.js
@@ -2,21 +2,24 @@ import { createStore } from 'redux';
 import rootReducer from './reducers';
 import { persistStore } from 'redux-persist';
 
-// Due to the size of the cell state, the app really bogs down Redux devtools,
-// even for other apps that are using Redux. commenting it out for production build.
+// Due to the size of the cell state, the app really bogs down Redux devtools.
+// Use stateSanitizer to prevent the performance issue.
 
 import { composeWithDevTools } from 'redux-devtools-extension';
 
-const composeEnhancers = composeWithDevTools({ trace: true, traceLimit: 25 });
+const stateSanitizer = (state) =>
+  state.life.cells
+    ? { ...state, life: { ...state.life, cells: '<<LONG_BLOB>>' } }
+    : state;
+
+const composeEnhancers = composeWithDevTools({
+  trace: true,
+  traceLimit: 25,
+  stateSanitizer,
+});
 
 export default () => {
   let store = createStore(rootReducer, composeEnhancers());
   let persistor = persistStore(store);
   return { store, persistor };
 };
-
-// export default () => {
-//   let store = createStore(rootReducer);
-//   let persistor = persistStore(store);
-//   return { store, persistor };
-// };
