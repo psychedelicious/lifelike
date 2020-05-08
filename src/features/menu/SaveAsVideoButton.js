6212748fac995d55893f7a67183ba115d466d40d
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2020-05-08 19:08:33 +0700

    Feature: implement pause & resume video recording

--- src/features/menu/SaveAsVideoButton.js
+++ src/features/menu/SaveAsVideoButton.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { IoMdVideocam } from 'react-icons/io';
+import { IoMdVideocam, IoMdPlay, IoMdPause } from 'react-icons/io';
 import { MdFiberManualRecord } from 'react-icons/md';
 
 import { IconButton } from '@chakra-ui/core';
 
 export const SaveAsVideoButton = ({ canvasBaseLayerRef }) => {
-  const [isRecordingVideo, setIsRecordingVideo] = React.useState(false);
+  const [recordingState, setRecordingState] = React.useState('stopped');
 
   const recorder = React.useRef();
   const stream = React.useRef();
@@ -36,12 +36,14 @@ export const SaveAsVideoButton = ({ canvasBaseLayerRef }) => {
         videoBitsPerSecond: 10000000,
         mimeType,
       }); // init the recorder
+
       // every time the recorder has new data, we will store it in our array
       recorder.current.ondataavailable = (e) =>
         e.data && e.data.size > 0 && chunks.push(e.data);
       // only when the recorder stops, we construct a complete Blob from all the chunks
       recorder.current.onstop = (e) =>
         exportVid(new Blob(chunks, { type: 'video/webm' }));
+
       recorder.current.start();
     },
     [exportVid]
@@ -49,27 +51,69 @@ export const SaveAsVideoButton = ({ canvasBaseLayerRef }) => {
 
   const handleStartCapture = React.useCallback(() => {
     startRecording(canvasBaseLayerRef.current);
-    setIsRecordingVideo(true);
+    setRecordingState('recording');
   }, [startRecording, canvasBaseLayerRef]);
 
   const handleStopCapture = React.useCallback(() => {
-    setIsRecordingVideo(false);
     recorder.current.stop();
+    setRecordingState('stopped');
+  }, []);
+
+  const handlePauseCapture = React.useCallback(() => {
+    recorder.current.pause();
+    setRecordingState('paused');
+  }, []);
+
+  const handleResumeCapture = React.useCallback(() => {
+    recorder.current.resume();
+    setRecordingState('recording');
   }, []);
 
   return (
-    <IconButton
-      icon={isRecordingVideo ? MdFiberManualRecord : IoMdVideocam}
-      fontSize="1.5rem"
-      p={0}
-      h="unset"
-      minW="unset"
-      mr="0.5rem"
-      variant="link"
-      variantColor={isRecordingVideo ? 'red' : 'blue'}
-      aria-label="save as video"
-      onClick={isRecordingVideo ? handleStopCapture : handleStartCapture}
-    />
+    <>
+      <IconButton
+        icon={
+          recordingState === 'recording' || recordingState === 'paused'
+            ? MdFiberManualRecord
+            : IoMdVideocam
+        }
+        fontSize="1.5rem"
+        p={0}
+        h="unset"
+        minW="unset"
+        mr={recordingState !== 'stopped' ? '0.25rem' : '0.5rem'}
+        variant="link"
+        variantColor={
+          recordingState === 'recording' || recordingState === 'paused'
+            ? 'secondary'
+            : 'blue'
+        }
+        aria-label="save as video"
+        onClick={
+          recordingState === 'recording' || recordingState === 'paused'
+            ? handleStopCapture
+            : handleStartCapture
+        }
+      />
+      {(recordingState === 'recording' || recordingState === 'paused') && (
+        <IconButton
+          icon={recordingState === 'recording' ? IoMdPause : IoMdPlay}
+          fontSize="1.5rem"
+          p={0}
+          h="unset"
+          mr="0.5rem"
+          minW="unset"
+          variant="link"
+          variantColor="blue"
+          aria-label="pause/resume recording"
+          onClick={
+            recordingState === 'recording'
+              ? handlePauseCapture
+              : handleResumeCapture
+          }
+        />
+      )}
+    </>
   );
 };
 
